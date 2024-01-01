e8c92227d306e32ef5ab8bf04381b8df4a344f61
vikassharma9029 <v.sharma9029@gmail.com>
2024-01-01 22:15:08 +0530

    create my profile pages fixes

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
-import FirstPage from './pages/First/FirstPage';
+// import FirstPage from './pages/First/FirstPage';
 import MyProfile from './Profile/MyProfile';
 import { BrowserRouter } from 'react-router-dom'
 
